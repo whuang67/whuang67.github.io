eb26b1a6475aeeb1ebfcef09cbe5992414745535
whuang67 <whuang67@illinois.edu>
2018-01-12 20:52:07 -0600

    updates sample works

---
+++ js/scoreKeeper_script.js
@@ -0,0 +1,58 @@
+var gameOver = false;
+var playerOneScore = 0;
+var playerTwoScore = 0;
+var winningScore = 5;
+var playerOneWin = 0;
+var playerTwoWin = 0;
+
+$("#playerOne").on("click", function(){
+	if(!gameOver){
+		playerOneScore++;
+		if(playerOneScore === winningScore){
+            $("#firstRecord").css("display", "none");
+			gameOver = true;
+			playerOneWin++;
+			$("#p1Display").css("color", "green");
+			$(".results").append("<li>"+(playerOneWin+playerTwoWin)+". <strong>Player 1</strong> won!</li>");
+			$("h3 span").text(playerOneWin+":"+playerTwoWin);
+		}
+		$("#p1Display").text(playerOneScore);
+	} else {
+		gameOver = true;
+	}
+});
+$("#playerTwo").on("click", function(){
+	if(!gameOver){
+		playerTwoScore++;
+		if(playerTwoScore === winningScore){
+			$("#firstRecord").css("display", "none");
+			gameOver = true;
+			playerTwoWin++;
+			$("#p2Display").css("color", "green");
+			$(".results").append("<li>"+(playerOneWin+playerTwoWin)+". <strong>Player 2</strong> won!</li>");
+			$("h3 span").text(playerOneWin+":"+playerTwoWin);
+		}
+		$("#p2Display").text(playerTwoScore);
+	} else {
+		gameOver = true;
+	}
+});
+
+
+
+function reset(){
+	gameOver = false;
+	playerOneScore = 0;
+	playerTwoScore = 0;
+	$("#p1Display").text(0);
+	$("#p2Display").text(0);
+	$("#p1Display").css("color", "black");;
+	$("#p2Display").css("color", "black");;
+}
+$("#reset").on("click", reset);
+
+$("input[type='number']").on("change", function(){
+	$("p span").text($(this).val());
+	winningScore = Number($(this).val());
+	reset();
+});
