2f5779df2d7fdd6d106e30b6a018960233ba7652
whuang67 <whuang67@illinois.edu>
2018-01-17 22:32:18 -0600

    updates refiles

--- js/index_script.js
+++ js/index_script.js
@@ -1,59 +1,11 @@
-$("#home").click(function(){
-	$(this).addClass("selected");
-	$("#aboutMe").removeClass("selected");
-	$("#sampleWork").removeClass("selected");
-	$("#contact").removeClass("selected");
-	
-	$("#homePanel").css("display", "block");
-	$("#aboutMePanel").css("display", "none");
-	$("#sampleWorkPanel").css("display", "none");
-	$("#contactPanel").css("display", "none");
-});
-
-$("#aboutMe").click(function(){
-	$("#home").removeClass("selected");
-	$(this).addClass("selected");
-	$("#sampleWork").removeClass("selected");
-	$("#contact").removeClass("selected");
-	
-	$("#homePanel").css("display", "none");
-	$("#aboutMePanel").css("display", "block");
-	$("#sampleWorkPanel").css("display", "none");
-	$("#contactPanel").css("display", "none");
-});
-
-$("#sampleWork").click(function(){
-	$("#home").removeClass("selected");
-	$("#aboutMe").removeClass("selected");
-	$(this).addClass("selected");
-	$("#contact").removeClass("selected");
-	
-	$("#homePanel").css("display", "none");
-	$("#aboutMePanel").css("display", "none");
-	$("#sampleWorkPanel").css("display", "block");
-	$("#contactPanel").css("display", "none");
-});
-
-$("#contact").click(function(){
-	$("#home").removeClass("selected");
-	$("#aboutMe").removeClass("selected");
-	$("#sampleWork").removeClass("selected");
-	$(this).addClass("selected");
-	
-	$("#homePanel").css("display", "none");
-	$("#aboutMePanel").css("display", "none");
-	$("#sampleWorkPanel").css("display", "none");
-	$("#contactPanel").css("display", "block");
-});
-
 $("#moreNewsBtn").click(function(){
-	$(this).addClass("notShowing");
-	$(".moreNews").removeClass("notShowing");
-	$("#lessNewsBtn").removeClass("notShowing");
+	$(this).css("display", "none");
+	$(".moreNews").css("display", "list-item");;
+	$("#lessNewsBtn").css("display", "block");;
 });
 
 $("#lessNewsBtn").click(function(){
-	$("#moreNewsBtn").removeClass("notShowing");
-	$(".moreNews").addClass("notShowing");
-	$(this).addClass("notShowing");
+	$("#moreNewsBtn").css("display", "block");
+	$(".moreNews").css("display", "none");
+	$(this).css("display", "none");
 });
